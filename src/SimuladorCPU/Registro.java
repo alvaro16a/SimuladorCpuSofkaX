303ec1b3b920632c12891fb421adbed55ab2bdd6
Alvaro Mena <alvaro16a@gmail.com>
2022-02-09 15:05:42 -0500

    Se creo el package simuladorCPU y se creo la clase Registro

---
+++ src/SimuladorCPU/Registro.java
@@ -0,0 +1,24 @@
+package SimuladorCPU;
+
+public class Registro {
+    long valor;
+
+    public void Registro(){
+        this.valor=0;
+    }
+
+    public long getValor() {
+        return valor;
+    }
+
+    public void setValor(long valor) {
+        if(valor > 4294967295L){
+            this.valor=0;
+        }else if(valor < 0){
+            this.valor=4294967295L;
+        }else{
+            this.valor=valor;
+        }
+        //this.valor = valor;
+    }
+}
